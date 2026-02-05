2f87f2035fed35b5c0e31409b3c1bfdf0b51f3a2
ritikawadaskar144 <ritikawadaskar144@gmail.com>
2026-02-05 16:26:48 +0530

    Add files via upload

--- addnu.java
+++ addnu.java
@@ -5,3 +5,17 @@ class AddNumbers {
         System.out.println("Sum = " + sum);
     }
 }
+
+
+class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
+
+
