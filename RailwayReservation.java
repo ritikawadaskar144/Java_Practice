36e5c44b6421cadd66d8d64dcb3252ad6825765c
ritikawadaskar144 <ritikawadaskar144@gmail.com>
2026-03-31 21:41:57 +0530

    Unit4_Q17

---
+++ RailwayReservation.java
@@ -0,0 +1,22 @@
+class RailwayReservation {
+    int seats = 1;
+
+    synchronized void bookTicket() {
+        if (seats > 0) {
+            System.out.println("Ticket booked successfully");
+            seats--;
+        } else {
+            System.out.println("No tickets available");
+        }
+    }
+
+    public static void main(String[] args) {
+        RailwayReservation r = new RailwayReservation();
+
+        Thread t1 = new Thread(() -> r.bookTicket());
+        Thread t2 = new Thread(() -> r.bookTicket());
+
+        t1.start();
+        t2.start();
+    }
+}
